a852d31fddf18000d8e2086450fb2287a112890c
aldavies <aldavies@umich.edu>
2017-07-17 21:02:49 -0400

    firebase examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireModule, FirebaseApp } from 'angularfire2';
+import {initializeApp, database } from 'firebase';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-}
+  nameList$: FirebaseListObservable<any>;
+  individual$: FirebaseObjectObservable<any>;
+
+  firstName: any;
+
+  constructor(private af: AngularFireDatabase) {
+    this.nameList$ = af.list('/names')
+    this.individual$ = af.object('/names/1')
+
+    
+    this.nameList$.subscribe(
+      val => console.log(val)
+    )
+
+    this.individual$.subscribe(
+      val => console.log(val)
+    )
+
+    this.nameList$.map(names => names[(names.length - 1)])
+      .subscribe(
+        name => this.firstName = name
+      )
+      // (function (names) {
+      //  return names[2];
+      // });
+      // Map allows you to work with a collective set of items and apply some logic to it. Whether you'd like to
+      // multiply values or grab a singular object out of it like the above illustrates
+  }
+  
+  listPush() {
+    this.nameList$.push({text: 'Rakesh'})
+  }
+
+  listRemove() {
+    this.nameList$.remove(this.firstName)
+  }
+
+  listUpdate() {
+    this.nameList$.update(this.firstName, {text: 'John Smith'})
+  }
+
+  objUpdate() {
+    this.individual$.update({text: 'Adam The Truth Davies'})
+  }
+
+  objSet() {
+    this.individual$.set({text: 'Adam The Semi Truth Davies'})
+  }
+  ngOnInit(){
+    
+  }
+
+    
+  }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, FirebaseApp } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCOt7IjXqoZxNQefdDheaxQb-aNxCs1HFc",
+  authDomain: "fir-ex-22c1a.firebaseapp.com",
+  databaseURL: "https://fir-ex-22c1a.firebaseio.com",
+  projectId: "fir-ex-22c1a",
+  storageBucket: "fir-ex-22c1a.appspot.com",
+  messagingSenderId: "923514308656"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
